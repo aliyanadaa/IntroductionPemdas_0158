98d62f6aee2cb172afd095b4f5d3ca6730a8fd1a
aliyanadaa <aliyanadaq@gmail.com>
2026-02-23 15:33:43 +0700

    first commit

---
+++ coba.cpp
@@ -0,0 +1,16 @@
+//contoh library
+#include <iostream>
+using namespace std;
+
+//variable globa;
+string nama;
+
+int main (){ //program utama
+    int age; //variable local
+    age = 20;
+
+    cout << "Halo world" << endl;
+    cout << 99 << endl;
+    cout << age;
+
+}
